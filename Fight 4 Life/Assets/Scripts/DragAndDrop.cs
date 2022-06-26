5d6cff8fee86ab758dc758aedac47f0813117a70
ItsMeBobbin <108188188+ItsMeBobbin@users.noreply.github.com>
2022-06-26 15:47:19 +0200

    NuovaScena/DragAndDrop
    Funziona tutto correttamente, per il drag and drop bisognerebbe trovare un modo per fare ritornare l'oggetto nella sua posizione originale se non è stata lasciata sopra il personaggio.

---
+++ Fight 4 Life/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool isDragging;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+
+    //Lo script funziona, ma bisogna ancora trovare un modo per farla ritornare alla sua posizione se non droppata sul personaggio
+}
